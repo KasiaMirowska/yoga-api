82866a8695739096b3de68fb4401623d44796e3a
Kasia <kasia@miroartdesign.com>
2020-01-03 09:30:07 -0500

    deploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
     'migrationsDirectory': 'migrations',
     'driver': 'pg',
-    'connectionString': process.env.DB_URL,
+    'connectionString': process.env.DATABASE_URL,
+    'ssl': !!process.env.SSL,
 }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const knex = require('knex');
 const app = require('../src/app');
-const { PORT , DB_URL } = require('./config');
+const { PORT , DATABASE_URL } = require('./config');
 
 
 const db = knex({
     client: 'pg',
-    connection: DB_URL,
+    connection: DATABASE_URL,
 })
 app.set('db', db);
 
