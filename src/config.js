51749a93089063202770cf3e99118880fb9f9fa8
Kasia <kasia@miroartdesign.com>
2020-01-14 15:31:23 -0500

    g

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://kamirska@localhost/yoga-app',
-    JWT_SECRET: process.env.JWT_SECRET || 'yoga-secret',
+    JWT_SECRET: process.env.JWT_SECRET || 'yoga-nowysekret',
     CLIENT_ORIGIN: 'https://yoga-capstone.kasia2.now.sh'
 }
